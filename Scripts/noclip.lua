84f66412736c3e95c0da5bbf19a1b524f74429ef
Jérémy Bolduc <BoldJe1.work@gmail.com>
2020-12-20 16:29:10 -0500

    Various optimisations and reformating

--- Scripts/noclip.lua
+++ Scripts/noclip.lua
@@ -9,51 +9,34 @@ MOVE_DOWN_KEY = 46
 CHANGE_SPEED_KEY = 21
 MOVE_LEFT_RIGHT = 30
 MOVE_UP_DOWN = 31
-VEHICLE_NEXT_RADIO = 81
+NOCLIP_TOGGLE_KEY = 81
 NO_CLIP_NORMAL_SPEED = 0.5
 NO_CLIP_FAST_SPEED = 2.5
 ENABLE_TOGGLE_NO_CLIP = true
+STARTUP_STRING = ('%s v%s initialized'):format(GetCurrentResourceName(), GetResourceMetadata(GetCurrentResourceName(), 'version', 0))
 
 -- Variables --
 local isNoClipping = false
 local playerPed = PlayerPedId()
 local playerId = PlayerId()
-local target = playerPed
 local speed = NO_CLIP_NORMAL_SPEED
 local input = vector3(0, 0, 0)
 local previousVelocity = vector3(0, 0, 0)
-local currentPosition = GetEntityCoords(target, true)
-local justExitedNoClip = false;
 local breakSpeed = 10.0;
+local offset = vector3(0, 0, 1);
 
-function ToggleNoClipMode()
+function ToggleNoClipMode() return SetNoClip(not isNoClipping) end
 
-    SetNoClip(not isNoClipping)
+function IsControlAlwaysPressed(inputGroup, control) return IsControlPressed(inputGroup, control) or IsDisabledControlPressed(inputGroup, control) end
 
-end
+function IsControlAlwaysJustPressed(inputGroup, control) return IsControlJustPressed(inputGroup, control) or IsDisabledControlJustPressed(inputGroup, control) end
 
-function IsControlAlwaysPressed(inputGroup, control)
-
-    return IsControlPressed(inputGroup, control) or IsDisabledControlPressed(inputGroup, control)
-
-end
-
-function IsControlAlwaysJustPressed(inputGroup, control)
-
-    return IsControlJustPressed(inputGroup, control) or IsDisabledControlJustPressed(inputGroup, control);
-
-end
+function Lerp (a, b, t) return a + (b - a) * t end
 
 function SetInvincible(val)
 
-    SetEntityInvincible(target, val)
-    SetPlayerInvincible(target, val)
-
-end
-
-function LerpVector3 (a, b, t)
-
-    return vector3(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t, a.z + (b.z - a.z) * t)
+    SetEntityInvincible(playerPed, val)
+    return SetPlayerInvincible(playerPed, val)
 
 end
 
@@ -64,28 +47,45 @@ function SetNoClip(val)
         isNoClipping = val;
 
         SetUserRadioControlEnabled(not isNoClipping)
-        FreezeEntityPosition(target, isNoClipping)
-        ClearPedTasksImmediately(target)
+        FreezeEntityPosition(playerPed, isNoClipping)
+        ClearPedTasksImmediately(playerPed)
 
         if (isNoClipping) then
 
-            SetInvincible(true);
-            currentPosition = GetEntityCoords(target, true)
-            justExitedNoClip = true;
-
-        else
-
-            -- On attend d'atterrir avant d'enlever l'invincibilité du joueur --
+            -- Start a No CLip thread
             Citizen.CreateThread(function()
 
-                while justExitedNoClip and not isNoClipping do
+                -- We start with no-clip mode because of the above if --
+                SetInvincible(true);
+
+                while isNoClipping do
 
                     Citizen.Wait(0);
 
-                    if (GetEntityHeightAboveGround(target) <= 2) and not IsPedFalling(target) then
+                    -- `(a and b) or c`, is basically `a ? b : c` --
 
-                        SetInvincible(false);
-                        justExitedNoClip = false;
+                    input = vector3(GetControlNormal(0, MOVE_LEFT_RIGHT), GetControlNormal(0, MOVE_UP_DOWN), (IsControlAlwaysPressed(1, MOVE_UP_KEY) and 1) or ((IsControlAlwaysPressed(1, MOVE_DOWN_KEY) and -1) or 0))
+                    speed = (IsControlAlwaysPressed(1, CHANGE_SPEED_KEY) and NO_CLIP_FAST_SPEED) or NO_CLIP_NORMAL_SPEED
+
+                    MoveInNoClip();
+
+                end
+
+                -- We're done with the while so we aren't in no-clip mode anymore --
+
+                -- Wait until the player starts falling or is completely stopped --
+                while (IsPedStopped(playerPed) or not IsPedFalling(playerPed)) and not isNoClipping do
+                    Citizen.Wait(0);
+                end
+
+                while not isNoClipping do
+
+                    Citizen.Wait(0);
+
+                    if IsPedStopped(playerPed) and not IsPedFalling(playerPed) then
+
+                        -- We hit land. We can safely remove the invincibility --
+                        return SetInvincible(false);
 
                     end
 
@@ -101,15 +101,13 @@ end
 
 function MoveInNoClip()
 
-    local rotX, rotY, rotZ = table.unpack(GetGameplayCamRot(0))
-    SetEntityRotation(target, rotX, rotY, rotZ, true)
-    local forward, right, up, c = GetEntityMatrix(target)
+    SetEntityRotation(playerPed, GetGameplayCamRot(0), 0, true)
+    local forward, right, up, c = GetEntityMatrix(playerPed)
     -- On utilise le système de coordonné du ped pour le faire bouger --
 
-    local velocity = LerpVector3(previousVelocity, (((right * input.x * speed) + (up * -input.z * speed) + (forward * -input.y * speed))), Timestep() * breakSpeed);
-    c = c + velocity
-    SetEntityCoords(target, c.x, c .y, c.z - 1, true, true, true, false)
-    previousVelocity = velocity
+    previousVelocity = Lerp(previousVelocity, (((right * input.x * speed) + (up * -input.z * speed) + (forward * -input.y * speed))), Timestep() * breakSpeed);
+    c = c + previousVelocity
+    SetEntityCoords(playerPed, c - offset, true, true, true, false)
 
 end
 
@@ -118,34 +116,16 @@ AddEventHandler('playerSpawned', function()
 
     playerPed = PlayerPedId()
     playerId = PlayerId()
-    target = playerPed;
 
 end)
 
 Citizen.CreateThread(function()
 
-    print(('%s v%s initialized'):format(GetCurrentResourceName(), GetResourceMetadata(GetCurrentResourceName(), 'version', 0)))
+    print(STARTUP_STRING)
 
-    while true do
+    while ENABLE_TOGGLE_NO_CLIP do
         Citizen.Wait(0)
-
-        if ENABLE_TOGGLE_NO_CLIP and (IsControlAlwaysJustPressed(1, VEHICLE_NEXT_RADIO)) then
-
-            ToggleNoClipMode()
-
-        end
-
-        if (isNoClipping) then
-
-            -- `(a and b) or c`, c'est l'équivalent de `a ? b : c` --
-
-            input = vector3(GetControlNormal(0, MOVE_LEFT_RIGHT), GetControlNormal(0, MOVE_UP_DOWN), (IsControlAlwaysPressed(1, MOVE_UP_KEY) and 1) or ((IsControlAlwaysPressed(1, MOVE_DOWN_KEY) and -1) or 0))
-            speed = (IsControlAlwaysPressed(1, CHANGE_SPEED_KEY) and NO_CLIP_FAST_SPEED) or NO_CLIP_NORMAL_SPEED
-
-            MoveInNoClip();
-
-        end
-
+        if IsControlAlwaysJustPressed(1, NOCLIP_TOGGLE_KEY) then ToggleNoClipMode() end
     end
 
 end)
