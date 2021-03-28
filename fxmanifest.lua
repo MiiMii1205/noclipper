714311532101a2c8538f7bfbf8b4e00c4c1cf585
Jérémy Bolduc <BoldJe1.work@gmail.com>
2021-03-28 13:17:15 -0400

    Added a repository field in the fxmanifest.lua file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ game 'gta5'
 author 'MiiMii1205'
 description 'A pleasant, simple and fast NoClip script for FiveM'
 version '1.3.0'
+repository 'https://github.com/MiiMii1205/noclipper'
 
 client_script 'Scripts/noclip.lua'
 export 'ToggleNoClipMode'
